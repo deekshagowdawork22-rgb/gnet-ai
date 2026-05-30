cd78d1e7b92f4c40486b01bf02ce0dbe2c5a4438
Deeksha Gowda <deekshagowda.work22@gmail.com>
2026-05-30 12:46:26 +0530

    fix railway port issue

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.post("/chat", (req, res) => {
 // ======================
 // Start Server
 // ======================
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
